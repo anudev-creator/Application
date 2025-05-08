21e37815c82096246614f7cb37de222793b39567
anudev-creator <159486828+anudev-creator@users.noreply.github.com>
2025-05-08 12:59:41 +0530

    last blog

--- Bloglist/DBCardData.js
+++ Bloglist/DBCardData.js
@@ -2,49 +2,49 @@ exports.BlogDataCard = [
     {
         Heading: "Game hacking: Assult Cube",
         pera: "In this write-up, well explore the fundamentals of game hacking using Assault Cube as our target. Well start by learning how to identify dynamic addresses for elements like health and ammo and then progress to finding their static counterparts. Once we have the addresses, well create a simple Lua script to automate value manipulation.",
-        img: "{{baseUrl}}/blogBanner/new.png",
+        img: "{{baseUrl}}/blogBanner/blog1.png",
         Date: "24-02-2024",
         Pera1: "In this write-up, well explore the fundamentals of game hacking using Assault Cube as our target."
     },
     {
         Heading: "XYZ router Command Injection ",
         pera: "we conduct extensive security assessments on IoT devices. In this article, we are presenting a command injection bug we discovered in the firmware of an XYZ IoT device, along with the root cause of the issue. The device is referred to as XYZ throughout this article because we have not received a proper patch from the vendor to date, and the vendor is not happy about us publishing it.",
-        img: "{{baseUrl}}/blogBanner/img_1.jpg",
+        img: "{{baseUrl}}/blogBanner/blog2.png",
         Date: "24-02-2024",
         Pera1: "we conduct extensive security assessments on IoT devices."
     },
     {
         Heading: "Automated Security Testing....",
         pera: "In today's fast-paced digital landscape, software security is no longer just a technical necessity—it's a strategic business imperative. A single vulnerability can lead to catastrophic data breaches, legal liabilities, and reputational damage.",
-        img: "{{baseUrl}}/blogBanner/img_2.jpg",
+        img: "{{baseUrl}}/blogBanner/blog3.png",
         Date: "24-02-2024",
         Pera1: "In today's fast-paced digital landscape, software security is no longer just a technical necessity—it's a strategic business imperative."
     },
     {
         Heading: "Uncovering SSRF Vulnerabilities ...",
         pera: "discuss an advanced topic concerning PDF generation in applications and the critical vulnerabilities associated with it. You’re all familiar with this well-known common vulnerability, but I just wanted to share it with you.",
-        img: "{{baseUrl}}/blogBanner/img_4.jpg",
+        img: "{{baseUrl}}/blogBanner/blog4.png",
         Date: "24-02-2024",
         Pera1: "discuss an advanced topic concerning PDF generation in applications"
     },
     {
         Heading: "Use-After-Free: A Small Oversight, A Hacker’s Dream",
         pera: "This article takes an attacker's perspective on the Use-After-Free (UAF) vulnerability class, moving beyond generic explanations found on wiki pages. According to the CWE Top 25 Most Dangerous Software Weaknesses of 2024, UAF ranks 8th if we exclude web application vulnerabilities ....",
-        img: "{{baseUrl}}/blogBanner/img_5.jpg",
+        img: "{{baseUrl}}/blogBanner/blog5.png",
         Date: "24-02-2024",
         Pera1: "This article takes an attacker's perspective on the Use-After-Free (UAF) vulnerability class"
     },
     {
         Heading: "Exploiting Command Injection in a Router Web Interface",
         pera: "During a security assessment of a router's web interface, a critical command injection vulnerability was discovered in the Ping Test feature. This flaw allowed arbitrary system commands to be executed on the router, potentially leading to full system compromise....",
-        img: "{{baseUrl}}/blogBanner/img_6.png",
+        img: "{{baseUrl}}/blogBanner/blog6.png",
         Date: "24-02-2024",
         Pera1: "During a security assessment of a router's web interface, a critical command injection vulnerability was discovered in the Ping Test feature"
     },
     {
         Heading: "AWS S3 Bucket Security Test Cases",
         pera: "This document outlines essential security test cases for AWS S3 buckets, ensuring data confidentiality, integrity, and availability. These tests aim to identify and mitigate potential vulnerabilities that could expose sensitive information or disrupt service. ",
-        img: "{{baseUrl}}/blogBanner/img_8.jpg",
+        img: "{{baseUrl}}/blogBanner/blog7.png",
         Date: "24-02-2024",
         Pera1: "This document outlines essential security test cases for AWS S3 buckets, ensuring data confidentiality, integrity, and availability. "
     }
